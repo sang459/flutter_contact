7945715dcf1c2adae15d64e1a12a83a0898a6b51
sang459 <sang459@gmail.com>
2023-02-09 17:56:39 +0900

    hawawa

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,37 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: ListView(
+          children: [
+            hawawa, hawawa, hawawa
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+
+        ),
+      )
 
     );
 
+  }
+}
+
+var hawawa = Row(
+  children: [
+    Icon(Icons.contact_page_rounded),
+    Text('홍길동'),
+  ],
+)
+
+class ShopItem extends StatelessWidget {
+  const ShopItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      child: Text('gd'),
+    );
   }
 }
