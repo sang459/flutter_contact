b15932126b8bfd259cd1a08af0f36387f33f3375
sang459 <sang459@gmail.com>
2023-02-09 20:12:13 +0900

    ㄹㅇㅋㅋ

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,74 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  var a = 0;
 
   @override
   Widget build(BuildContext context) {
 
     return MaterialApp(
       home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Text(a.toString()),
+          onPressed: (){
+            a++;
+            print(a);
+          },
+        ),
         appBar: AppBar(),
-        body: ListView(
-          children: [
-            hawawa, hawawa, hawawa
-          ],
+        body: ListView.builder(
+          itemCount: 3,
+          itemBuilder: (context, i){
+            return hawawa;
+          },
         ),
-        bottomNavigationBar: BottomAppBar(
-
-        ),
+        bottomNavigationBar: BottomBarCus(),
       )
-
     );
 
   }
 }
 
-var hawawa = Row(
-  children: [
-    Icon(Icons.contact_page_rounded),
-    Text('홍길동'),
-  ],
-)
+var hawawa = Padding(
+  padding: const EdgeInsets.all(8.0),
+  child:   Row(
 
-class ShopItem extends StatelessWidget {
-  const ShopItem({Key? key}) : super(key: key);
+    children: [
+
+      Icon(Icons.contact_page_rounded),
+
+      Text('홍길동'),
+
+    ],
+
+  ),
+);
+
+class BottomBarCus extends StatelessWidget {
+  const BottomBarCus({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
-      child: Text('gd'),
+    return BottomAppBar(
+      child: SizedBox(
+        height: 50,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(Icons.phone),
+            Icon(Icons.message),
+            Icon(Icons.contact_page)
+          ],
+        ),
+      ),
+
+
     );
   }
 }
