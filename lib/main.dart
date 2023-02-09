25e8df74a6eb0ec86cd7bd959241da878fefbe75
sang459 <sang459@gmail.com>
2023-02-09 11:20:02 +0900

    제목높이지움

--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,38 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar( title: Text('피드'), ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image(image: AssetImage('sample.jpg'),),
-            Text('이것은당신의기타 이다')
-          ],
+        body: Container(
+          height: 200, width: double.infinity,
+          color: Color(0xff8a53ea),
+          padding: EdgeInsets.all(20),
+          child: Row(
+            children: [
+              Container(
+                color: Color(0xffacf1c9),
+                width: 150, height: 150,
+                child: Text('1'),
+              ),
+              Container(
+                color: Color(0xfffae48d),
+                width: 300, height: 150, //여기 width를 화면크기에 딱맞게 설정하는법?
+                child: Column(
+                  children: [
+                    Container(
+                        padding: EdgeInsets.all(10),
+                        child: Text('제목', style: )
+                    ),
+                    Text('성동구행당동 개지랄'),
+                    Text('69만원'),
+                    Icon(Icons.heart_broken)
+                  ],
+                )
+
+              )
+            ],
+
+          ),
         ),
+
         bottomNavigationBar: BottomAppBar(
           child: SizedBox(
             height: 50,
