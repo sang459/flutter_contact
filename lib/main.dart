d78b889e0c33049bc69561be062ccc1e5523d602
sang459 <sang459@gmail.com>
2023-02-18 17:02:47 +0900

    앱번들 만들기전

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:contacts_service/contacts_service.dart';
+
 
 void main() {
   runApp(
@@ -19,11 +22,45 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
-  var contactList = ['김병식'];
+  List<Contact> contactList = [];
+
+  getPermission() async {
+    var status = await Permission.contacts.status;
+    if (status.isGranted) {
+      print('허락됨');
+      var contacts = await ContactsService.getContacts();
+      setState(() {
+        contactList = contacts;
+      });
+
+
+      //print(contacts[0].displayName);
+      //var newPerson = Contact();
+      //newPerson.givenName = '민수';
+      //newPerson.familyName = '김';
+      //ContactsService.addContact(newPerson);
+
+
+
+    } else if (status.isDenied) {
+      print('거절됨');
+      Permission.contacts.request();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+  }
 
   addOne(text) {
     setState(() { ///이거 까먹지 마라!!!!!!!!!!!!
-      contactList.add(text);
+      var newPerson = Contact();
+      newPerson.givenName = text;
+      ContactsService.addContact(newPerson);
+
+      getPermission();
     });
   }
 
@@ -42,17 +79,27 @@ class _MyAppState extends State<MyApp> {
             );
             },
         ),
-        appBar: AppBar( title: Text('')),
+        appBar: AppBar( actions: [
+          IconButton(onPressed: (){ getPermission(); }, icon: Icon(Icons.contacts))
+        ] ),
         body: ListView.builder(
           itemCount: contactList.length,
           itemBuilder: (context, i) {
-            return Row(
-              children: [
-                Icon(Icons.contact_page_outlined),
-                Text(contactList[i])
-              ],
-            );
-          },
+            if (contactList[i].givenName != Null) {  // Null Check
+              return
+                Row(
+                  children: [
+                    Icon(Icons.contact_page_outlined, size: 50),
+                    Text(contactList[i].givenName!, style: TextStyle(fontSize: 50))   //!는 null이 아니라고 확신할 때 쓰는거.
+                  ],
+                );
+            }
+            else {
+              return SizedBox();
+            }
+          }
+
+
 
         )
 
@@ -90,7 +137,7 @@ class DialogUI extends StatelessWidget {
   Widget build(BuildContext context) {
     return Dialog(
         child: SizedBox(
-          height: 150, width: 300,
+          height: 200, width: 300,
           child: Column(
             children: [
               Flexible(
@@ -114,7 +161,7 @@ class DialogUI extends StatelessWidget {
               ),
               Flexible(
                   child: Padding(
-                      padding: EdgeInsets.all(15),
+                      padding: EdgeInsets.all(10),
                       child: Row(
                         crossAxisAlignment: CrossAxisAlignment.end,
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -125,12 +172,12 @@ class DialogUI extends StatelessWidget {
                             inputData = '';
                             Navigator.pop(context);
                             },
-                              child: Text('확인')),
+                              child: Text('OK')),
                           TextButton(
                               onPressed: (){
                                 Navigator.pop(context); 
                                 },
-                              child: Text('취소')),
+                              child: Text('Cancel')),
                         ],
                       )
                   )
@@ -142,3 +189,6 @@ class DialogUI extends StatelessWidget {
     );
   }
 }
+
+
+// C:\"Program Files"\Java\jdk-19\bin\keytool -genkey -v -keystore C:\Users\sanghyun\Documents\android_keys\upload-keystore.jks -storetype JKS -keyalg RSA -keysize 2048 -validity 10000 -alias upload
